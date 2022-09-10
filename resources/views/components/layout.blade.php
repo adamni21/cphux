fc7672c56ccc528e00a9ca875fb9b1b90085cae4
adamni21 <85580454+adamni21@users.noreply.github.com>
2022-09-10 17:49:37 +0100

    added livewire to layout component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,11 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Cphux</title>
+    @livewireStyles
     @vite('resources/css/app.css')
 </head>
 
 <body>
     {{ $slot }}
+    @livewireScripts
 </body>
 
 </html>
